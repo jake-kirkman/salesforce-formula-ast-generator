a75173140ada44e4f6e7cc1df1ff931335fab04c
Jake Kirkman <checko9@hotmail.co.uk>
2024-10-04 12:27:08 +0100

    Adding tokens for line numbers etc to type definitions

--- src/types/grouping.ts
+++ src/types/grouping.ts
@@ -1,9 +1,12 @@
+import Token from "../classes/token";
 import ExpressionType from "../enums/expression-type";
 import Expression from "./expression";
 import GroupMember from "./group-member";
 
 type Grouping = Expression<ExpressionType.GROUPING> & {
   Members: GroupMember[];
+  LeftBracket: Token;
+  RightBracket: Token;
   Wrapped: boolean;
 };
 export default Grouping;
--- src/types/literal.ts
+++ src/types/literal.ts
@@ -1,7 +1,9 @@
+import Token from "../classes/token";
 import ExpressionType from "../enums/expression-type";
 import Expression from "./expression";
 
 type Literal = Expression<ExpressionType.LITERAL> & {
   Value: string | number | boolean;
+  Token: Token;
 };
 export default Literal;
