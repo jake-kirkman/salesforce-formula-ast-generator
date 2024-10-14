c94ee6e59c733de8a57262a1ac773a4dca69c887
Jake Kirkman <checko9@hotmail.co.uk>
2024-10-14 14:29:53 +0100

    Types

---
+++ src/index.d.ts
@@ -0,0 +1,24 @@
+import Action from './types/action';
+import Binary from './types/binary';
+import Expression from './types/expression';
+import FormattingOptions from './types/formatting-options';
+import GroupMember from './types/group-member';
+import Grouping from './types/grouping';
+import Identifier from './types/identifier';
+import Literal from './types/literal';
+import Unary from './types/unary';
+import {format, parse} from './index';
+
+export {
+  Action,
+  Binary,
+  Expression,
+  FormattingOptions,
+  GroupMember,
+  Grouping,
+  Identifier,
+  Literal,
+  Unary,
+  format,
+  parse
+}
